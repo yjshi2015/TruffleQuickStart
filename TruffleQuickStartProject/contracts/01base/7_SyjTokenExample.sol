667ec99a228a32e037b6d3b57f9847c36fd62c1b
yjshi <495054021@qq.com>
2023-08-10 09:11:56 +0800

    发币

---
+++ TruffleQuickStartProject/contracts/01base/7_SyjTokenExample.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+/*
+发放代币token，在Remix中连上metamask钱包，然后deploy即可
+*/
+
+import "@ipenzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract SyjToken is ERC20 {
+
+    constructor() ERC20("SyjToken", "SYJ") {
+        _mint(msg.sender, 100000000 * 10 ** decimals());
+    }
+}
